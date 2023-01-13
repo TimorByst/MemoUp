541aadef641da2bd1974f885d1d78e977a1c0ae8
timor <timobystritskie@gmail.com>
2023-01-13 22:37:48 +0200

    initial commit

---
+++ app/src/main/java/com/example/memoup/Activity_Splash.java
@@ -0,0 +1,132 @@
+package com.example.memoup;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatImageView;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.textview.MaterialTextView;
+
+
+public class Activity_Splash extends AppCompatActivity {
+
+    private static final long DELAY_SPLASH_START = 1500;
+    private MaterialTextView splash_TXT_name;
+    private MaterialTextView splash_TXT_creator;
+    private AppCompatImageView splash_IMG_logo;
+    private Animation slide_left_to_right;
+    private Animation slide_right_to_left;
+    private Animation fade_in;
+    private final String CREATOR_NAME = "By Beast Games ltd. ©";
+    private final String APP_NAME = "MemoUp";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        hideSystemUI(this);
+        findViews();
+        initViews();
+        playSplashAnimationWithDelay();
+
+    }
+
+    private void findViews() {
+        splash_TXT_creator = findViewById(R.id.splash_TXT_creator);
+        splash_TXT_name = findViewById(R.id.splash_TXT_name);
+        splash_IMG_logo = findViewById(R.id.splash_IMG_logo);
+    }
+
+    private void initViews() {
+        splash_TXT_creator.setText(CREATOR_NAME);
+        splash_TXT_name.setText(APP_NAME);
+        Glide.with(this).load(R.drawable.memo_up_logo).into(splash_IMG_logo);
+
+        slide_right_to_left = AnimationUtils.loadAnimation(
+                this, R.anim.slide_in_right_to_left);
+        slide_left_to_right = AnimationUtils.loadAnimation(
+                this, R.anim.slide_in_left_to_right);
+        fade_in = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+
+
+    }
+
+    private void playSplashAnimationWithDelay() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                playSplashAnimation();
+            }
+        }, DELAY_SPLASH_START);
+    }
+
+    void playSplashAnimation() {
+        splash_TXT_name.setVisibility(View.VISIBLE);
+        splash_IMG_logo.setVisibility(View.INVISIBLE);
+        splash_TXT_creator.setVisibility(View.INVISIBLE);
+        slide_right_to_left.setAnimationListener(
+                new MyAnimationListener(
+                        slide_left_to_right,
+                        splash_IMG_logo,
+                        null, Activity_Splash.this));
+        slide_left_to_right.setAnimationListener(
+                new MyAnimationListener(
+                        fade_in,
+                        splash_TXT_creator,
+                        null, Activity_Splash.this));
+        fade_in.setAnimationListener(
+                new MyAnimationListener(
+                        null,
+                        null,
+                        new Intent(
+                                Activity_Splash.this,
+                                Activity_Login.class),
+                        Activity_Splash.this));
+
+        splash_TXT_name.startAnimation(slide_right_to_left);
+    }
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI(this);
+        }
+    }
+
+    public static void hideSystemUI(Activity activity) {
+        // Enables regular immersive mode.
+        // For "lean back" mode, remove SYSTEM_UI_FLAG_IMMERSIVE.
+        // Or for "sticky immersive," replace it with SYSTEM_UI_FLAG_IMMERSIVE
+        View decorView = activity.getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        // Set the content to appear under the system bars so that the
+                        // content doesn't resize when the system bars hide and show.
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN
+                        // Dim the Status and Navigation Bars
+                        | View.SYSTEM_UI_FLAG_LOW_PROFILE);
+
+        // Without - cut out display
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            activity.getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/memoup/MyAnimationListener.java
@@ -0,0 +1,44 @@
+package com.example.memoup;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.view.animation.Animation;
+
+class MyAnimationListener implements Animation.AnimationListener {
+
+    private Animation nextAnimation;
+    private View nextView;
+    private Intent intent;
+    private Activity activity;
+
+    public MyAnimationListener(Animation nextAnimation, View nextView, Intent intent, Activity activity) {
+        this.nextAnimation = nextAnimation;
+        this.nextView = nextView;
+        this.intent = intent;
+        this.activity = activity;
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        if(nextAnimation != null){
+            if(nextView.getVisibility() == View.INVISIBLE){
+                nextView.setVisibility(View.VISIBLE);
+            }
+            nextView.startAnimation(nextAnimation);
+        }
+        if (intent != null) {
+            activity.startActivity(intent);
+            activity.finish();
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+    }
+}
