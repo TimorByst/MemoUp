997681db335bc26fd26776cfb03edbcff1cd297a
timor <timobystritskie@gmail.com>
2023-01-15 17:02:55 +0200

    Auth commit

---
+++ app/src/main/java/com/example/memoup/Activity_CreateUser.java
@@ -0,0 +1,14 @@
+package com.example.memoup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_CreateUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+    }
+}
--- app/src/main/java/com/example/memoup/Activity_Login.java
+++ app/src/main/java/com/example/memoup/Activity_Login.java
@@ -1,9 +1,12 @@
 package com.example.memoup;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
@@ -11,8 +14,16 @@ import android.view.WindowManager;
 import android.widget.EditText;
 
 import com.bumptech.glide.Glide;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
 import com.google.android.material.textfield.TextInputLayout;
 import com.google.android.material.textview.MaterialTextView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class Activity_Login extends AppCompatActivity {
 
@@ -20,6 +31,7 @@ public class Activity_Login extends AppCompatActivity {
     private EditText username_TXT_login;
     private MaterialTextView headline_TXT_login;
     private AppCompatImageView avatar_IMG_login;
+    private FirebaseAuth myAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +41,46 @@ public class Activity_Login extends AppCompatActivity {
         findViews();
         initViews();
 
+        myAuth = FirebaseAuth.getInstance();
+        FirebaseUser user = myAuth.getCurrentUser();
+
+        if(user == null){
+            prettyLogin();
+        }else{
+            // TO DO
+            // add logic for known user
+        }
+
+
+
+    }
+
+    private void prettyLogin(){
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.PhoneBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.TwitterBuilder().build());
+
+        // Create and launch sign-in intent
+        Intent signInIntent = AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build();
+        signInLauncher.launch(signInIntent);
+    }
+
+    private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
+            new FirebaseAuthUIActivityResultContract(),
+            new ActivityResultCallback<FirebaseAuthUIAuthenticationResult>() {
+                @Override
+                public void onActivityResult(FirebaseAuthUIAuthenticationResult result) {
+                    onSignInResult(result);
+                }
+            }
+    );
+
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
     }
 
     private void findViews(){
